6c8d4d944ea100be1e2bf85b4027fa36a8b6576f
Ruta Joshi <rjoshi@berkeley.edu>
2016-11-12 00:02:02 -0800

    Added openCV template code and tf training data template code

--- scratch.py
+++ scratch.py
@@ -1,4 +1,27 @@
 
+
+# NOTE: this is OpenCV code we need to modify.
+# It activates the webcam and starts reading frames
+import cv2
+
+cv2.namedWindow("preview")
+vc = cv2.VideoCapture(0)
+if vc.isOpened(): # try to get the first frame
+    rval, frame = vc.read()
+else:
+    rval = False
+    while rval:
+        cv2.imshow("preview", frame)
+        rval, frame = vc.read()
+        key = cv2.waitKey(20)
+        if key == 27: # exit on ESC
+            break
+cv2.destroyWindow("preview")
+
+
+# NOTE: this is tf code that takes a dataset as a set of filenames and trains a model on it
+# This is basic format for the code, so we need to add our own data and get it to work
+
 # step 1
 filenames = ['im_01.jpg', 'im_02.jpg', 'im_03.jpg', 'im_04.jpg']
 
