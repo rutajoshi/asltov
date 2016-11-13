1f93676af08215a06d7593e33ecd70f671759c72
Ruta Joshi <rjoshi@berkeley.edu>
2016-11-13 09:36:33 -0800

    Changed file types to png

--- scratch.py
+++ scratch.py
@@ -1,42 +1,56 @@
 
-
-# NOTE: this is OpenCV code we need to modify.
-# It activates the webcam and starts reading frames
-import cv2
-
-cv2.namedWindow("preview")
-vc = cv2.VideoCapture(0)
-if vc.isOpened(): # try to get the first frame
-    rval, frame = vc.read()
-else:
-    rval = False
-    while rval:
-        cv2.imshow("preview", frame)
-        rval, frame = vc.read()
-        key = cv2.waitKey(20)
-        if key == 27: # exit on ESC
-            break
-cv2.destroyWindow("preview")
-
-
 # NOTE: this is tf code that takes a dataset as a set of filenames and trains a model on it
 # This is basic format for the code, so we need to add our own data and get it to work
+from os import listdir
+from os.path import isfile, join
+import tensorflow as tf
+
+mypath = '/Users/Ruta/Developer/calhacks3/asltov/imageRecogData'
 
 # step 1
-filenames = ['im_01.jpg', 'im_02.jpg', 'im_03.jpg', 'im_04.jpg']
+filenames = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+print("Finished step 1")
 
 # step 2
 filename_queue = tf.train.string_input_producer(filenames)
+print("Finished step 2")
 
 # step 3: read, decode and resize images
 reader = tf.WholeFileReader()
 filename, content = reader.read(filename_queue)
-image = tf.image.decode_jpeg(content, channels=3)
-image = tf.cast(image, tf.float32)
-resized_image = tf.image.resize_images(images, 224, 224)
+print("Read the filename queue")
 
-# step 4: Batching
-image_batch = tf.train.batch([resized_image], batch_size=8)
+my_img = tf.image.decode_png(content) # use png or jpg decoder based on your files.
+print("Decoded images")
 
+init_op = tf.initialize_all_variables()
+print("Initialized all vars")
+sess = tf.Session()
+with sess.as_default():
+    sess.run(init_op)
+    print("Ran the init op")
+    # Start populating the filename queue.
+    coord = tf.train.Coordinator()
+    threads = tf.train.start_queue_runners(coord=coord)
+    print("Started populating filename queue")
 
+    for i in range(10): #length of your filename list
+        print("Evaluating image #" + str(i))
+        image = my_img.eval() #here is your image Tensor :)
 
+    print(image.shape)
+    Image.show(Image.fromarray(np.asarray(image)))
+
+    coord.request_stop()
+    coord.join(threads)
+
+# image = tf.image.decode_jpeg(content, channels=3)
+# image = tf.cast(image, tf.float32)
+# resized_image = tf.image.resize_images(image, 224, 224)
+# images.append(resized_image)
+#
+# print("Finished step 3")
+#
+# # step 4: Batching
+# image_batch = tf.train.batch(images, batch_size=8)
+# print("Finished step 4")
